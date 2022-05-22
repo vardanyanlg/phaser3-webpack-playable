9ac167938a732f88c77e397e54f59c678c6246e9
Levon Vardanyan <37041198+vardanyanlg@users.noreply.github.com>
2022-05-22 23:02:13 +0300

    Add chaining to load functions

--- src/js/PreloaderScene.js
+++ src/js/PreloaderScene.js
@@ -19,27 +19,29 @@ export default class PreloaderScene extends Phaser.Scene {
     preload () {
         console.log('%cSCENE::PreloaderScene', 'color: #fff; background: #f00;')
 
-        this.addAudio('sound_fx', sound_fxA);
+        this
+            .addAudio('sound_fx', sound_fxA)
+            .addTexture('bg', bgI)
 
-        this.addTexture('bg', bgI);
-
-        this.textures.on('onload', () => countDecodedTexture++, this);
-        this.sound.on('decoded', () => countDecodedAudio++, this);
+        this.textures.on('onload', () => countDecodedTexture++, this)
+        this.sound.on('decoded', () => countDecodedAudio++, this)
     }
     addAudio(arg1,arg2) {
-        allAudio++;
-        this.sound.decodeAudio(arg1, arg2);
+        allAudio++
+        this.sound.decodeAudio(arg1, arg2)
+        return this
     }
     addTexture(arg1,arg2) {
-        allTextures++;
-        this.textures.addBase64(arg1, arg2);
+        allTextures++
+        this.textures.addBase64(arg1, arg2)
+        return this
     }
     create() {
         loadMain = true
     }
    update() {
         if (loadMain && (countDecodedAudio >= allAudio) && (countDecodedTexture >= allTextures)) {
-            console.log('%cSCENE::Loaded', 'color: #000; background: #0f0;');
+            console.log('%cSCENE::Loaded', 'color: #000; background: #0f0;')
             this.scene.start("Main")
         }
     }
